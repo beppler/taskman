230109cacebcb3ea848fd67b55438df9e9362341
Carlos Alberto Costa Beppler <beppler@gmail.com>
2021-04-25 03:06:34 -0300

    Centralize the login vertically.

--- app/pages/login.php
+++ app/pages/login.php
@@ -36,28 +36,32 @@ $title = 'Login';
 require(__DIR__ . '/_header.php');
 ?>
 
-<div class="row align-items-center justify-content-center">
-    <div class="col-4">
-        <h1 class="text-center">Login</h1>
-    </div>
-</div>
-<div class="row align-items-center justify-content-center">
-    <div class="col-4">
-        <form method="post">
-            <div class="form-group">
-                <label for="username" class="<?php echo isset($formErrors['username']) ? 'text-danger' : '' ?>">Username:</label>
-                <input type="text" name="username" id="username" autofocus class="form-control <?php echo isset($formErrors['username']) ? 'is-invalid' : '' ?>" value="<?php echo htmlspecialchars($username, ENT_QUOTES) ?>">
-                <div class="invalid-feedback"><?php echo $formErrors['username'] ?? ''?></div>
+<div class="row row-flex align-items-center h-100">
+    <div class="col">
+        <div class="row justify-content-center">
+            <div class="col">
+                <h1 class="text-center">Login</h1>
             </div>
-            <div class="form-group">
-                <label for="password" class="<?php echo isset($formErrors['password']) ? 'text-danger' : '' ?>">Password:</label>
-                <input type="password" name="password" id="password" class="form-control <?php echo isset($formErrors['password']) ? 'is-invalid' : '' ?>" value="<?php echo htmlspecialchars($password, ENT_QUOTES) ?>">
-                <div class="invalid-feedback"><?php echo $formErrors['password'] ?? ''?></div>
+        </div>
+        <div class="row justify-content-center">
+            <div class="col-4">
+                <form method="post">
+                    <div class="form-group">
+                        <label for="username" class="<?php echo isset($formErrors['username']) ? 'text-danger' : '' ?>">Username:</label>
+                        <input type="text" name="username" id="username" autofocus class="form-control <?php echo isset($formErrors['username']) ? 'is-invalid' : '' ?>" value="<?php echo htmlspecialchars($username, ENT_QUOTES) ?>">
+                        <div class="invalid-feedback"><?php echo $formErrors['username'] ?? ''?></div>
+                    </div>
+                    <div class="form-group">
+                        <label for="password" class="<?php echo isset($formErrors['password']) ? 'text-danger' : '' ?>">Password:</label>
+                        <input type="password" name="password" id="password" class="form-control <?php echo isset($formErrors['password']) ? 'is-invalid' : '' ?>" value="<?php echo htmlspecialchars($password, ENT_QUOTES) ?>">
+                        <div class="invalid-feedback"><?php echo $formErrors['password'] ?? ''?></div>
+                    </div>
+                    <div class="form-group text-center">
+                        <button class="btn btn-primary" type="submit">Submit</button>
+                    </div>
+                </form>
             </div>
-            <div class="form-group text-center">
-                <button class="btn btn-primary" type="submit">Submit</button>
-            </div>
-        </form>
+        </div>
     </div>
 </div>
 
