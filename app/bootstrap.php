15c6175eb0cf32d1b083b20fda10e9e87ea69fcd
Carlos Alberto Costa Beppler <beppler@gmail.com>
2022-02-16 09:30:33 -0300

    Remove last slash on BASE_URL.

--- app/bootstrap.php
+++ app/bootstrap.php
@@ -23,7 +23,7 @@ set_exception_handler(function (Throwable $exception) {
     include(__DIR__ . '/pages/error.php');
 });
 
-defined('BASE_URL') or define('BASE_URL', (empty($_SERVER['HTTPS']) ? "http" : "https") . '://' . $_SERVER['HTTP_HOST'] . '/');
+defined('BASE_URL') or define('BASE_URL', (empty($_SERVER['HTTPS']) ? "http" : "https") . '://' . $_SERVER['HTTP_HOST']);
 
 defined('APP_DIR') or define('APP_DIR', realpath(__DIR__));
 defined('DATA_DIR') or define('DATA_DIR', realpath(__DIR__ . '/../data'));
